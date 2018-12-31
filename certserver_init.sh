6c0f863de266eafb2c870dcf893c07b904e205b2
maxspad <maxspad@umich.edu>
2018-12-31 14:25:56 -0500

    cleaned out shell scripts

--- certserver_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install software-properties-common
-add-apt-repository universe
-add-apt-repository ppa:certbot/certbot
-apt-get update
-apt-get install certbot 
--- clean_proj.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -rf secrets/ work/ Dockerfile .gitignore
