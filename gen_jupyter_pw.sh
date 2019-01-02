6fc87339e96551cb7fc94cfa4e5ba4f4cd431bd3
maxspad <maxspad@umich.edu>
2019-01-02 05:28:39 +0000

    now it just asks for password on startup

--- gen_jupyter_pw.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-echo "Placing jupyter password hash in secrets/jupyterpw.txt"
-docker run -it jupyter/datascience-notebook python -c "from notebook.auth import passwd; print(passwd())" | tail -n 1 > secrets/jupyterpw.txt
--- start_notebook.sh
+++ start_notebook.sh
@@ -3,18 +3,20 @@
 if [ $1 = "server" ]
 then
     echo "Starting on server..."
-    docker run --rm \
+    docker run --rm -it \
         -p 443:8888 \
         -v /root/dock-datasci/work:/home/jovyan/work \
         -v /root/dock-datasci/secrets:/home/jovyan/secrets \
         notebook \
+        /bin/bash -c " \
+        jupyter notebook password; \
         start.sh jupyter notebook \
-            --NotebookApp.password="$(cat /root/dock-datasci/secrets/jupyterpw.txt)" \
             --certfile /home/jovyan/secrets/fullchain.pem \
-            --keyfile /home/jovyan/secrets/privkey.pem
+            --keyfile /home/jovyan/secrets/privkey.pem"
 fi
 
 if [ $1 = "local" ]
 then
     echo "local"
 fi
+#            #--NotebookApp.password="$(cat /root/dock-datasci/secrets/jupyterpw.txt)" \
