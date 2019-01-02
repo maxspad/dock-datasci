880d0f20b9c30855e9253e7a3350a57f4db2d206
namxspad <maxspad@umich.edu>
2019-01-01 23:06:52 -0500

    init_server, provis_server, gen_pw first drafts

--- init_server.sh
+++ init_server.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-apt-get update
-apt-get install git
+# Install git
+apt-get -y update
+apt-get -y install git
+
+# Get our code repo
 git clone https://github.com/maxspad/dock-datasci
 cd dock-datasci
+
+# Make the secrets directory (filled later)
+mkdir secrets
+
+# Build our image
 docker build -t notebook notebooks/
--- provis_server.sh
+++ provis_server.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# Get rid of error from floating IP
+ssh-keygen -f "/home/max/.ssh/known_hosts" -R "steponmy.com"
+
+# Push our server init.sh script up
+scp init_server.sh root@steponmy.com:/root/
+
+# Run it
+ssh root@steponmy.com /bin/bash /root/init_server.sh
+
+# Image is built, push secret keys/passwords 
+scp secrets/ root@steponmy.com:/root/dock-datasci/secrets/
