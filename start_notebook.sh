8c6c29bdb92a2678ca629bffb2e3f000af1f311e
maxspad <maxspad@umich.edu>
2019-01-03 00:20:56 +0000

    permissions fixed?

--- start_notebook.sh
+++ start_notebook.sh
@@ -2,19 +2,22 @@
 
 if [ $1 = "server" ]
 then
+    echo "Setting perimssions..."
+    chmod -R 777 secrets/
+    chmod -R 777 work/
     echo "Starting on server..."
     docker run --rm -it \
         -p 443:8888 \
         -v /root/dock-datasci/work:/home/jovyan/work \
-        -v /root/dock-datasci/secrets:/home/jovyan/secrets \
+        -v /root/dock-datasci/secrets:/etc/ssl/notebook/ \
         -e CHOWN_HOME=yes \
         -e CHOWN_HOME_OPTS='-R' \
         notebook \
         /bin/bash -c " \
         jupyter notebook password; \
         start.sh jupyter notebook \
-            --certfile /home/jovyan/secrets/fullchain.pem \
-            --keyfile /home/jovyan/secrets/privkey.pem"
+            --certfile /etc/ssl/notebook/fullchain.pem \
+            --keyfile /etc/ssl/notebook//privkey.pem"
 fi
 
 if [ $1 = "local" ]
