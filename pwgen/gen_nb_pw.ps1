413ef4c4bb5caab9f39f3c0dea0ae2dbb1f57816
maxspad <maxspad@umich.edu>
2019-01-04 16:43:25 -0500

    Added script/dockerfile to generate password hash

---
+++ pwgen/gen_nb_pw.ps1
@@ -0,0 +1,4 @@
+echo "Generating password in docker container..."
+docker build -t nb-pw-generator .
+docker run --rm -it --mount "type=bind,src=$(pwd),dst=/home/jovyan" nb-pw-generator
+mv nbpw.txt ..\secrets
