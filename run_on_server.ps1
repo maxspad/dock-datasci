ec68e772eaca8346c5f030153ee1b546a08f9062
maxspad <maxspad@umich.edu>
2019-01-05 13:27:29 -0500

    Added run_on_server.ps1

---
+++ run_on_server.ps1
@@ -0,0 +1,4 @@
+# 
+ssh root@steponmy.com "git clone http://github.com/maxspad/dock-datasci"
+scp -r .\secrets root@steponmy.com:~/dock-datasci/secrets 
+ssh root@steponmy.com "cd ~/dock-datasci && docker-compose up --build"
