e427fdf2df50e8a8a389c6f3fa57b2ccafa5b3af
maxspad <maxspad@umich.edu>
2019-01-06 13:24:50 -0500

    Startup script directory-agnostic, server-agnostic. Removed 'old' dir

--- run_on_server.ps1
+++ run_on_server.ps1
@@ -1,5 +1,19 @@
 # 
-ssh root@steponmy.com "git clone http://github.com/maxspad/dock-datasci"
-scp -r .\secrets root@steponmy.com:~/dock-datasci/secrets 
-ssh root@steponmy.com "cd ~/dock-datasci && docker-compose up -d --build"
-echo "Server should be running!"
+
+param (
+    [string]$server = "steponmy.com",
+    [string]$username = "root",
+    [string]$githubuser = "maxspad"
+)
+
+# Get repository name 
+$repo_name = $(Get-Item $PSScriptRoot).Name
+Write-Output "Server: $server"
+Write-Output "Username: $username"
+Write-Output "GithubUser: $githubuser"
+Write-Output "Repo name: $repo_name"
+
+ssh "$username@$server" "git clone https://github.com/$githubuser/$repo_name"
+scp -r .\secrets "${username}@${server}:~/${repo_name}/secrets"
+ssh "$username@$server" "cd ~/$repo_name && docker-compose up -d --build"
+Write-output "Server should be running!"
